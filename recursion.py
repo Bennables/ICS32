7839a31f84be397f9e0a2b7dcc6a1dcce4527de4
Ben Sirivallop <bensirivallop@dhcp-10-8-016-138.mobile.reshsg.uci.edu>
2024-06-04 13:04:14 -0700

    recursion for adding each digit of number

--- recursion.py
+++ recursion.py
@@ -14,3 +14,13 @@ def expo(num, exp):
         return num * expo(num,exp-1)
     
 print(expo(3,5))
+
+#add each digit of number
+
+def adddigits(num):
+    if num == 0:
+        return 0
+    else:
+        return num%10 +adddigits(num//10)
+
+print(adddigits(1234))
