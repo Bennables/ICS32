03d098991c37c1f7b94cb9e999d5714b7c6d3816
Ben Sirivallop <bensirivallop@dhcp-10-8-016-138.mobile.reshsg.uci.edu>
2024-06-04 13:02:35 -0700

    recursion for exponents

--- recursion.py
+++ recursion.py
@@ -5,3 +5,12 @@ def factoria(a):
         return(a*factoria(a-1))
 
 print(factoria(5))
+
+#exponent recursion
+def expo(num, exp):
+    if exp == 0:
+        return 1
+    else:
+        return num * expo(num,exp-1)
+    
+print(expo(3,5))
